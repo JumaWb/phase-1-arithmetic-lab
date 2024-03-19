74e229a5aed30a67ac1df07389deea1fd81ccf74
Brian Juma <bjuma0985@gmail.com>
2024-03-20 00:02:36 +0300

    doing arthmetics

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+console.log(multiply);
 // Write your code here
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random)
+const num3 = 32
+const num4 = 7
+const mod = num3 % num4;
+console.log(mod);
+const numbers = [5, 12, 14, 16, 18, 20]
+let max = numbers [0]
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+        max = numbers[i];
+    }
+}
+console.log(max);
